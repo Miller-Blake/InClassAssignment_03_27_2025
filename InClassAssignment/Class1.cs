6b0282b6b47d4cebb17403182cb2d52c993be821
Miller-Blake <blakemiller5464@gmail.com>
2025-03-27 11:46:06 -0400

    Add project files.

---
+++ InClassAssignment/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InClassAssignment
+{
+    internal class Assignment
+    {
+        private String _name;
+
+        static void assignmentAuthors()
+        {
+            Console.WriteLine("This assignment was created by Blake Miller and Gabe Galiatsatos");
+        }
+
+        public string Name { get { return _name; } set { _name = value; } }
+    }
+}
